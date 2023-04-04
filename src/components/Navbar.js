a3a98c912e1b39e629a3558bbec5adfed9a47c96
GilesAlonso <86449189+GilesAlonso@users.noreply.github.com>
2023-04-04 12:23:16 -0300

    updated github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/GilesAlonso/gilesalonso.github.io"
                 target="_blank"
                 className="fork-btn-inner"
               >
