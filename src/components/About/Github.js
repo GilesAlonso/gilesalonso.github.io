d2fa490fa73de74299692042da20d8e13227142c
GilesAlonso <86449189+GilesAlonso@users.noreply.github.com>
2023-04-04 14:40:43 +0000

    updated files

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="soumyajit4419"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#a2f584"
         fontSize={16}
       />
     </Row>
