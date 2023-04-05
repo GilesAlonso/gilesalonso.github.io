16dd8b0241bb09eb5680dc402b5c2afe9efb3251
GilesAlonso <86449189+GilesAlonso@users.noreply.github.com>
2023-04-05 13:38:30 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -85,7 +85,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/resume"
+                to="https://resume.io/r/gwekWX6T3"
                 onClick={() => updateExpanded(false)}
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
